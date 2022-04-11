4c700746e265cadea2c8ac3f4388b4f4b3cf8684
FernandoPFH <51247535+FernandoPFH@users.noreply.github.com>
2022-04-10 21:47:37 -0300

    Colocado Os Boost No Mapa1

--- FarahZero/Assets/Scripts/AlinharBoostComAPista.cs
+++ FarahZero/Assets/Scripts/AlinharBoostComAPista.cs
@@ -8,17 +8,34 @@ public class AlinharBoostComAPista : MonoBehaviour
 {
     public bool Alinhar = false;
     public LayerMask LayerMaskParaAlinhar;
+    
+    public float LocalRotationOffSet = 0f;
+    private float _ultimoValor_LocalRotationOffSet = 0f;
 
     private void OnDrawGizmos()
     {
         if (Alinhar)
-            if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hitInfo, 30f,LayerMaskParaAlinhar))
-            {
-                transform.up = hitInfo.normal;
+            CalcularPosicaoERotacaoDoBoost();
 
-                transform.RotateAround(transform.position, transform.up, 180f);
+        if (LocalRotationOffSet != _ultimoValor_LocalRotationOffSet)
+        {
+            CalcularPosicaoERotacaoDoBoost();
 
-                transform.position = hitInfo.point;
-            }
+            _ultimoValor_LocalRotationOffSet = LocalRotationOffSet;
+        }
+    }
+
+    private void CalcularPosicaoERotacaoDoBoost()
+    {
+        if (Physics.Raycast(transform.position + transform.up, -transform.up, out RaycastHit hitInfo, 30f,LayerMaskParaAlinhar))
+        {
+            transform.up = hitInfo.normal;
+
+            transform.RotateAround(transform.position, transform.up, 180f);
+
+            transform.RotateAround(transform.position, transform.up, LocalRotationOffSet);
+
+            transform.position = hitInfo.point;
+        }
     }
 }
